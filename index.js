48c0b282776b6f675fbf06b7e9b1cb1c76691343
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-04 12:32:11 +0500

    UPdate

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const h1 = document.createElement('h1')
-h1.textContent("Hello World")
-h1.className = "header"
-console.log(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
 
-const element = <h1 className='header'> ReactJS</h1>
-console.log(element)
+// const element = <h1 className="header">This is JSX</h1>
+// console.log(element)
 
-ReactDOM.render(element, document.getElementById('root'))
+// JSX
+const page = (
+    <div>
+        <h1>This is title of Paragraph</h1>
+        <p>
+            Mesopotamia (c. 3500–500 BCE): Often called the cradle of civilization,
+            it was home to the Sumerians, Akkadians, Babylonians, and Assyrians.
+            They developed writing (cuneiform), the wheel, and early forms of
+            government and law.
+        </p>
+    </div>
+)
+ReactDOM.render(page, document.getElementById("root"))
