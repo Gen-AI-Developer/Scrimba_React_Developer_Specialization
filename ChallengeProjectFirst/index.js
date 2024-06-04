6898bd45f5c289386e50aabb603a16d1ec6f370b
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-04 12:57:21 +0500

    Cc

--- ChallengeProjectFirst/index.js
+++ ChallengeProjectFirst/index.js
@@ -1,21 +1,30 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
+// Challenge: 
 
-// const element = <h1 className="header">This is JSX</h1>
-// console.log(element)
+// Create a navbar in JSX:
+//     - Use the semantic `nav` element as the parent wrapper
+//     - Have an h1 element with the brand name of your "website"
+//     - Insert an unordered list for the other nav elements
+//         - Inside the `ul`, have three `li`s for "Pricing",
+//         "About", and "Contact"
+//     - Don't worry about styling yet - it'll just be plain-looking HTML for now
 
-// JSX
-const page = (
-    <div className="border m-2 p-6 rounded shadow hover:bg-black">
-        <h1 className="text-3xl font-bold text-zinx-500 hover:text-white" >History of Civilization</h1>
-        <p className="text-lg mt-2 hover:text-white">
-            Mesopotamia (c. 3500–500 BCE): Often called the cradle of civilization,
-            it was home to the Sumerians, Akkadians, Babylonians, and Assyrians.
-            They developed writing (cuneiform), the wheel, and early forms of
-            government and law.
-        </p>
+
+const PageContent = (
+    <div>
+        <h1>this is other para</h1>
+        <p>This is a lurem IPsum Para</p>
     </div>
 )
-ReactDOM.render(page, document.getElementById("root"))
+
+const navPage = (
+    <nav>
+        <h1>Logistics</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+ReactDOM.render(navPage, document.getElementById("nav"))
+// ReactDOM.render(PageContent, document.getElementById("nav"))
