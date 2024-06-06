30a87a834a785f4780c93bf064ad17ece6849593
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-06 13:08:16 +0500

    UpdateLast

--- Modules01/ParentChildComponents/index.js
+++ Modules01/ParentChildComponents/index.js
@@ -1,10 +1,34 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
-
-// const element = <h1 className="header">This is JSX</h1>
-// console.log(element)
-
-// JSX
-ReactDOM.render(<h1>This is React</h1>, document.getElementById("root"))
+function Header() {
+    return (
+        <header>
+            <nav className=" border rounded p-4 flex">
+                <span><img className="animate-spin" src="./react-logo.png" width="40px" /> RT-Dev</span>
+            </nav>
+        </header>
+    )
+}
+function Footer() {
+    return (
+        <footer className="text-sm p-4 border rounded">
+            <small>© 2024 RT-Dev development. All rights reserved.</small>
+        </footer>
+    )
+}
+function Page() {
+    return (
+        <div className="m-3 p-3 border rounded shadow">
+            <Header />
+            <div className="mt-10 mb-10">
+                <h1 className="text-lg font-bold">Reasons I'm excited to learn React</h1>
+                <ol>
+                    <li>It's a popular library, so I'll be
+                        able to fit in with the cool kids!</li>
+                    <li>I'm more likely to get a job as a developer
+                        if I know React</li>
+                </ol>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+ReactDOM.render(<Page />, document.getElementById("root"))
