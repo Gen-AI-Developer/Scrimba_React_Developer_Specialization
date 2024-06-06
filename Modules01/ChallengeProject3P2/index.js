daba2b9899a42ee21fba4a7bbbd914a439d8b92f
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-06 12:23:11 +0500

    challenge3p2

---
+++ Modules01/ChallengeProject3P2/index.js
@@ -0,0 +1,32 @@
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+
+ */
+
+    // import React from "react"
+    // import ReactDOM from "react-dom"
+    
+    function Page() {
+        return (
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        )
+    }
+    
+    ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(navPage, document.getElementById("nav"))
+// ReactDOM.render(PageContent, document.getElementById("nav"))
