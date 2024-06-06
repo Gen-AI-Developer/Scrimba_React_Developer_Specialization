5bdf418a150a3a00388f9cd49c8cadce4101d5bc
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-06 12:39:29 +0500

    ChallengeCompleted

--- Modules01/ChallengeProject3P2/index.js
+++ Modules01/ChallengeProject3P2/index.js
@@ -13,20 +13,37 @@ Part 2:
 
  */
 
-    // import React from "react"
-    // import ReactDOM from "react-dom"
-    
-    function Page() {
-        return (
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        )
-    }
-    
-    ReactDOM.render(<Page />, document.getElementById("root"))
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+function Page() {
+    return (
+        <div>
+            <div>
+                <header>
+                    <nav>
+                        <img src="./react-logo.png" width={40} height={40} />
+                        <h1>React Challenge 3 Part 2</h1>
+                    </nav>
+                </header>
+            </div>
+            <div>
+                <ol>
+                    <li>It's a popular library, so I'll be
+                        able to fit in with the cool kids!</li>
+                    <li>I'm more likely to get a job as a developer
+                        if I know React</li>
+                </ol>
+            </div>
+            <div>
+                <footer>
+                    <center><em>© 2024 Accient React Development. All rights reserved."</em></center>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
 ReactDOM.render(navPage, document.getElementById("nav"))
 // ReactDOM.render(PageContent, document.getElementById("nav"))
