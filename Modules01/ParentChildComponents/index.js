bb7d8d646d0ca38ea03a6e4f696361cf07123965
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-06 12:51:01 +0500

    initialUpdate

---
+++ Modules01/ParentChildComponents/index.js
@@ -0,0 +1,10 @@
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+// const element = <h1 className="header">This is JSX</h1>
+// console.log(element)
+
+// JSX
+ReactDOM.render(<h1>This is React</h1>, document.getElementById("root"))
