828907d4596ab883c4f9762089f63e4c537630c1
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-08 16:30:36 +0500

    Update

---
+++ Modules01/Project-1st/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Main from './components/Main'
+
+const App = () => {
+    return (
+        <div className='container'>
+            <Nav />
+            <Main />
+
+        </div>
+    )
+}
+
+export default App
