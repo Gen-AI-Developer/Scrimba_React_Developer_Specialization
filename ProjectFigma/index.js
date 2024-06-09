eddb94200f76fdb25e1330f22ec512877c269918
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-09 08:53:57 +0500

    initialUpdate

--- ProjectFigma/index.js
+++ ProjectFigma/index.js
@@ -23,15 +23,16 @@ function Header() {
 }
 function MainContent() {
     return (
-        <div className="main-content">
-            <h1 className="raleway">Reasons I'm excited to learn React</h1>
-            <ol className="raleway-p o-list">
-                <li>It's a popular library, so I'll be
-                    able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                    if I know React</li>
-            </ol>
-        </div>
+        <main>
+            <h1>Fun Facts about React.</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 function Footer() {
