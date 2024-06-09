5484bb092a4975af7ed1f534b5058f993d66c45b
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-09 17:45:22 +0500

    CssUpdated

--- Modules01/Module01_FinalProject/App.js
+++ Modules01/Module01_FinalProject/App.js
@@ -1,12 +1,34 @@
 
 
-
-
+function Header() {
+    return (
+        <div>header</div>
+    )
+}
+function Footer() {
+    return (
+        <div>Footer</div>
+    )
+}
 
 
 function App() {
     return (
-        <div>App</div>
+        <div className="main--container">
+            <div className="first-div">
+                <div className="main--content">
+                    <img className="img" src="./profile-img.jpg" />
+                    <div>Safdar Ali Shah</div>
+                    <div>Full Stack Developer</div>
+                    <div>programmersafdar@live.com</div>
+                    <div>
+                        <button>Email</button>
+                        <button>LinkedIn</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
