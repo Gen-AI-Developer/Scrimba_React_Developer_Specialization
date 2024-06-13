8e5b92ee48ca3b49a98008f55a01394899e5ba1e
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-13 10:05:34 +0500

    UpdatedDoneProject

--- Modules01/Module01_FinalProject/App.js
+++ Modules01/Module01_FinalProject/App.js
@@ -1,61 +1,75 @@
+function Card() {
+    return (
+        <div className="card">
+            <div className="profile">
 
+            </div>
+            <div className="v0_185">
+
+            </div>
+        </div>
+
+    )
+}
 
 function Header() {
     return (
-        <div className="first-div">
-            <div className="">
-                <div className="">
-                    <img className="img" src="./profile-img.jpg" />
-                </div>
-
+        <div>
+            <div className="lead-txt">
+                <span className="v0_201">Safdar Ali</span>
+                <span className="v0_202">Full Stack Developer</span>
+                <span className="v0_203">programmersafdar@live.com</span>
             </div>
+            <div className="social-btns">
+                <div className="name">
 
+                </div>
+                <span className="v0_229">Email</span>
+            </div>
+            <div className="v0_233">
+                <div className="name">
+
+                </div>
+                <span className="v0_235">LinkedIn</span>
+            </div>
         </div>
+
     )
 }
 function Footer() {
     return (
-        <div>
-            <div className="footer">
-                <img className="img-com-prop" src="twitter.png" />
-                <img className="img-com-prop" src="facebook.png" />
-                <img className="img-com-prop" src="instagram.png" />
-                <img className="img-com-prop" src="github.png" />
-            </div>
-        </div>
+        <footer>
+            <div className="footer-bg"></div>
+            <div className="gh-logo"></div>
+            <div className="twitter-logo"></div>
+            <div className="insta-logo"></div>
+            <div className="linkin-logo"></div>
+        </footer>
+
     )
 }
 
-function MainContent() {
+function Main() {
     return (
-
-        <div>
-            <div className="text-name">Safdar Ali Shah</div>
-            <div className="text-job">Full Stack Developer</div>
-            <div className="text-email">programmersafdar@live.com</div>
-            <div className="buttons">
-                <button className="email-button">Email</button>
-                <button className="linkedin-button">LinkedIn</button>
+        <main>
+            <div className="v0_196">
+                <span className="v0_197">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</span>
+                <span className="v0_198">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</span>
+                <span className="v0_199">About</span>
+                <span className="v0_200">Interests</span>
             </div>
-            <div>
-                <div>About</div>
-                <div>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</div>
-                <div>Interests</div>
-                <div>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</div>
-            </div>
-        </div>
+        </main>
     )
 }
 
 
 function App() {
     return (
-        <div className="main--container">
-            <div className="main--content">
-                <Header />
-                <MainContent />
-                <Footer />
-            </div>
+        <div className="container">
+            <Card />
+            <Header />
+            <Main />
+            <Footer />
         </div>
     )
 }
