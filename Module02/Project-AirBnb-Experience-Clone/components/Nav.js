f599c4706871be20336a0d2051f9c2028b303997
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-21 08:29:52 +0500

    UpdatingNavBar

--- Module02/Project-AirBnb-Experience-Clone/components/Nav.js
+++ Module02/Project-AirBnb-Experience-Clone/components/Nav.js
@@ -5,6 +5,12 @@ export default function Nav() {
         <header>
             <nav className='nav'>
                 <img className='nav-logo' src="./images/airbnb-logo.png" />
+                {/* <ul>
+                    <li><a href="#about-us" class="nav-link">About Us</a></li>
+                    <li><a href="#videos" class="nav-link">Demo</a></li>
+                    <li><a href="#photos" class="nav-link">Photo Gallery</a></li>
+                    <li><a href="#contact-us" class="nav-link">Contact</a></li>
+                </ul> */}
             </nav>
         </header>
     )
