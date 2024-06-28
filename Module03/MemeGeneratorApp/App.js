3c5b466315c8ad1ae91045a81a7c9351b2ad635a
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-28 07:07:39 +0500

    initial git updated module03 meme generator application

---
+++ Module03/MemeGeneratorApp/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import NavBar from "./components/NavBar"
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return <main>
+        <NavBar />
+    </main>
+}
---
+++ Module03/MemeGeneratorApp/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+    return <header>
+        <nav>
+            <div>
+                <img className="nav-logo" src="./images/troll-face.png"></img>
+            </div>
+        </nav>
+    </header>
+}
