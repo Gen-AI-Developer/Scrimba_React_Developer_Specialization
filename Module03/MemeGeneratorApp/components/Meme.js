3579f760b4a2feb803f10437a00b91bf726abfcf
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-07-01 17:44:53 +0500

    Updated

--- Module03/MemeGeneratorApp/components/Meme.js
+++ Module03/MemeGeneratorApp/components/Meme.js
@@ -2,12 +2,8 @@ import React from "react";
 export default function Meme() {
     return <main className="form-main">
         <form className="form">
-            {/* <div className="form-div"> */}
-            {/* <label>Top Text</label> */}
             <input className="form-input" placeholder="Top Text" type="text"></input>
-            {/* <label>Bottom Text</label> */}
             <input className="form-input" placeholder="Bottom Text" type="text"></input>
-            {/* </div> */}
             <button>Get a new Meme Image 🖼</button>
         </form>
     </main>
