bae112094a4fcdab624233db6ae3acb84d606fbb
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-07-04 11:37:03 +0500

    UpdateTOday

--- Module03/ChallengeDotMapRender/index.js
+++ Module03/ChallengeDotMapRender/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/**
+    * Challenge: Map over the thingsArray to generate
+    * a <p> element for each item and render them on the page
+    * below the button
+    */
 
 function App() {
     const thingsArray = ["Thing 1", "Thing 2"]
-    
+
+
     return (
         <div>
             <button>Add Item</button>
+            {/* Insert the things here*/}
+            <p></p>
         </div>
     )
 }
