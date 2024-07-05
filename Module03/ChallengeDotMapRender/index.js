ab88d79b099f3868e308c81938276d5ac4753a28
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-07-05 14:45:05 +0500

    Updated

--- Module03/ChallengeDotMapRender/index.js
+++ Module03/ChallengeDotMapRender/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import data from '../../Module02/SoloProject_MyTravelJournal/data';
 /**
     * Challenge: Map over the thingsArray to generate
     * a <p> element for each item and render them on the page
     * below the button
     */
 
+function ParaElement([]) {
+    return
+}
+
 function App() {
     const thingsArray = ["Thing 1", "Thing 2"]
+    let paraelement = thingsArray.map((index) => {
+        return <p className='paragraph'>
+            
+        </p>
+    })
 
 
     return (
         <div>
-            <button>Add Item</button>
-            {/* Insert the things here*/}
-            <p></p>
+            <button onClick={e => {
+                const thingsArrayLength = thingsArray.length
+                thingsArray.push("Things ".concat(thingsArrayLength + 1))
+                // console.log(thingsArray)
+
+            }}>Add Item</button>
+            <div>
+                {paraelement}
+            </div>
         </div>
     )
 }
--- Module03/ChallengeDotMapRender/index.pack.js
+++ Module03/ChallengeDotMapRender/index.pack.js
@@ -444,7 +444,13 @@ var _reactDom = __webpack_require__(4);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
+var _data = __webpack_require__(15);
+
+var _data2 = _interopRequireDefault(_data);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
 
 /**
     * Challenge: Map over the thingsArray to generate
@@ -452,18 +458,35 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     * below the button
     */
 
+function ParaElement(_ref) {
+    var _ref2 = _toArray(_ref);
+
+    return;
+}
+
 function App() {
     var thingsArray = ["Thing 1", "Thing 2"];
+    var paraelement = thingsArray.map(function (index) {
+        return _react2.default.createElement('p', { className: 'paragraph' });
+    });
 
     return _react2.default.createElement(
         'div',
         null,
         _react2.default.createElement(
             'button',
-            null,
+            { onClick: function onClick(e) {
+                    var thingsArrayLength = thingsArray.length;
+                    thingsArray.push("Things ".concat(thingsArrayLength + 1));
+                    // console.log(thingsArray)
+                } },
             'Add Item'
         ),
-        _react2.default.createElement('p', null)
+        _react2.default.createElement(
+            'div',
+            null,
+            paraelement
+        )
     );
 }
 
@@ -30480,6 +30503,45 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ }),
+/* 15 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = [{
+    id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002532/img/basic/a0002532_main.jpg"
+}, {
+    id: 2,
+    title: "Sydney Opera House",
+    location: "AUSTRALIA",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "27 May, 2021 ",
+    endDate: "8 Jun, 2021",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+    imageUrl: "https://news.imz.at/imzfiles/737485-socialmediapreview-inside-the-sydney-opera-house-leads-abc-slate-9187868.jpg"
+}, {
+    id: 3,
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "https://cdn.sanity.io/images/4aans0in/production/21473a1b4f37cf766e24633ab471120c8f862ebf-2000x1500.jpg?w=3840&h=2880&fm=webp&q=100&fit=max&auto=format"
+}];
 
 /***/ })
 /******/ ]);
