046c6a1b4dda5ea9b9222892142dd08335bcce4a
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-07-11 08:40:10 +0500

    Updating UseState

--- Module03/counterApplication/App.js
+++ Module03/counterApplication/App.js
@@ -1,16 +1,24 @@
 import React from "react"
 
 export default function App() {
+
     /**
      * Challenge: Set up state to track our count (initial value is 0)
      */
+    let [count, setCount] = React.useState(342)
+    function add() {
+        setCount(preCount => preCount + 1)
+    }
+    function sub() {
+        setCount(preCount => preCount - 1)
+    }
     return (
         <div className="counter">
-            <button className="counter--minus">–</button>
+            <button className="counter--minus" onClick={sub} onTouchStart={sub}>–</button>
             <div className="counter--count">
-                <h1>0</h1>
+                <h1>{count}</h1>
             </div>
-            <button className="counter--plus">+</button>
+            <button className="counter--plus" onClick={add}>+</button>
         </div>
     )
 }
