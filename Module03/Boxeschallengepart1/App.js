95983051f699545ba30ddbad46daff445eaecfb1
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-08-03 23:40:00 +0500

    Updated

---
+++ Module03/Boxeschallengepart1/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import boxes from "./boxes"
+
+export default function App() {
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
+    return (
+        <main>
+            <h1>Boxes will go here</h1>
+        </main>
+    )
+}
